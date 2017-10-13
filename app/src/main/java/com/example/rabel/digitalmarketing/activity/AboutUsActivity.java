f0b54433e0d51618a2b07db3108968b8b0e6a70c
Rabel Ahmed <rabelhmd@gmail.com>
2017-10-13 15:42:24 +0600

    First Update

---
+++ app/src/main/java/com/example/rabel/digitalmarketing/activity/AboutUsActivity.java
@@ -0,0 +1,14 @@
+package com.example.rabel.digitalmarketing.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.rabel.digitalmarketing.R;
+public class AboutUsActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
